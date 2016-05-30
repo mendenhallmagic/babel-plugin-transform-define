d43bace851a015f73bce16847adc77688f8d50d9
Drew Mendenhall <mendenhallmagic@gmail.com>
2016-05-30 01:53:33 -0500

    Add identifier replacement

--- modules/index.js
+++ modules/index.js
@@ -2,6 +2,32 @@ export default function ({ types: t }) {
   return {
     visitor: {
 
+      Identifier(path, state) {
+        const replacements = state.opts
+        const keys = Object.keys(replacements)
+
+        for (let i = 0, len = keys.length; i < len; ++i) {
+          const key = keys[i]
+
+          if (path.node.name === key) {
+            const replacement = replacements[key]
+
+            path.replaceWith(replacement === '' ?
+              t.valueToNode(replacement) : t.identifier(replacement)
+            )
+
+            if (path.parentPath.isBinaryExpression()) {
+              const result = path.parentPath.evaluate()
+
+              if (result.confident)
+                path.parentPath.replaceWith(t.valueToNode(result.value))
+            }
+
+            break
+          }
+        }
+      },
+
       // process.env.NODE_ENV
       MemberExpression(path, state) {
         const replacements = state.opts
